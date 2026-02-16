506c34d8ed61987c793067103fbf800e5a41d612
Chrysolite Nocon and Jiro 'Jade Sergeant' Mendador <pair+cnocon+jmendador@spins.com>
2026-02-15 20:15:44 -0600

    [Phase 3] Permission Based Access

--- src/types/types-categories/typesCategoriesSearchTypes.ts
+++ src/types/types-categories/typesCategoriesSearchTypes.ts
@@ -2,6 +2,7 @@ import z from "zod";
 
 export const typesCategoriesSearchSchema = z.object({
   service: z.string(),
+  page: z.number().default(1),
 });
 
 export type typesCategoriesSearchData = z.infer<typeof typesCategoriesSearchSchema>;
